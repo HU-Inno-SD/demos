683b01047343477c60a0a790c442fdf6aea94824
Tom Kemper <tom.kemper@hu.nl>
2022-08-24 10:35:50 +0200

    Vers uit de initializr

---
+++ basicboot/src/main/java/nl/hu/sd/inno/basicboot/BasicbootApplication.java
@@ -0,0 +1,13 @@
+package nl.hu.sd.inno.basicboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicbootApplication.class, args);
+	}
+
+}
---
+++ basicboot/src/test/java/nl/hu/sd/inno/basicboot/BasicbootApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hu.sd.inno.basicboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
